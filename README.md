8c12695fafb2aa649ed99cf855a6b7042d8d42fe
Max Braun <max.braun@gmail.com>
2023-11-18 20:56:33 -0500

    Add image classification (on TPU) to use in prompt
    - Add MobileNet model (quantized and compiled for Edge TPU)
    - Add ImageNet label file
    - Implement image capture from the camera
    - Implement TensorFlow Lite inference on the TPU
    - Use the classification label in the story prompt

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ A "large" language model running on a microcontroller.
 
 ## Background
 
-I was wondering if it's possible to fit a non-trivial language model on a microcontroller. Turns out the answer is some version of yes!
+I was wondering if it's possible to fit a non-trivial language model on a microcontroller. Turns out the answer is some version of yes! (Later, things got a bit out of hand and now the prompt is based on objects detected by the camera.)
 
-This project is using the [Coral Dev Board Micro](https://coral.ai/products/dev-board-micro) with its [FreeRTOS toolchain](https://coral.ai/docs/dev-board-micro/freertos/). The board has a number of neat [hardware features](https://coral.ai/docs/dev-board-micro/get-started/#the-hardware) not currently being used here (notably a [TPU](https://coral.ai/technology/), sensors, and a [second CPU core](https://coral.ai/docs/dev-board-micro/multicore/)). It does, however, also have 64MB of RAM. That's tiny for LLMs, which are typically measured in the GBs, but comparatively huge for a microcontroller. Inference runs on the 800 MHz [Arm Cortex-M7](https://developer.arm.com/Processors/Cortex-M7) CPU core.
+This project is using the [Coral Dev Board Micro](https://coral.ai/products/dev-board-micro) with its [FreeRTOS toolchain](https://coral.ai/docs/dev-board-micro/freertos/). The board has a number of neat [hardware features](https://coral.ai/docs/dev-board-micro/get-started/#the-hardware), but – most importantly for our purposes – it has 64MB of RAM. That's tiny for LLMs, which are typically measured in the GBs, but comparatively huge for a microcontroller.
 
 The LLM implementation itself is an adaptation of [llama2.c](https://github.com/karpathy/llama2.c) and the [tinyllamas](https://huggingface.co/karpathy/tinyllamas/tree/main) checkpoints trained on the [TinyStories](https://huggingface.co/datasets/roneneldan/TinyStories) dataset. The quality of the smaller model versions isn't ideal, but good enough to generate somewhat coherent (and occasionally weird) stories.
+
+Language model inference runs on the 800 MHz [Arm Cortex-M7](https://developer.arm.com/Processors/Cortex-M7) CPU core. Camera image classification uses the [TPU](https://coral.ai/technology/) and a [compiled MobileNet model](https://coral.ai/models/image-classification/). The board also has a second 400 MHz [Arm Cortex-M4](https://developer.arm.com/Processors/Cortex-M4) CPU core, which is currently unused.
 
 ## Setup
 
@@ -33,7 +35,7 @@ pip install -r coralmicro/scripts/requirements.txt
 
 ```
 
-Download the model and quantize it:
+Download the Llama model and quantize it:
 
 ```bash
 LLAMA_MODEL_NAME=stories15M
@@ -42,6 +44,15 @@ wget -P data https://huggingface.co/karpathy/tinyllamas/resolve/main/${LLAMA_MOD
 python llama2.c/export.py data/${LLAMA_MODEL_NAME}_q80.bin --version 2 --checkpoint data/${LLAMA_MODEL_NAME}.pt
 
 cp llama2.c/tokenizer.bin data/
+```
+
+Download the vision model and its labels:
+
+```bash
+VISION_MODEL_NAME=tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu
+wget -P data https://raw.githubusercontent.com/google-coral/test_data/master/${VISION_MODEL_NAME}.tflite
+
+wget -P data https://raw.githubusercontent.com/google-coral/test_data/master/imagenet_labels.txt
 ```
 
 Build and flash the image:
@@ -58,12 +69,13 @@ python ../coralmicro/scripts/flashtool.py --build_dir . --elf_path llama4micro
 
 ## Usage
 
-1. The model loads automatically when the board powers up.
-   - This takes ~6 seconds.
-   - The green light will turn on when it's ready.
-2. Press the button next to the green light.
+1. The models load automatically when the board powers up.
+   - This takes ~9 seconds.
+   - The green light will turn on when ready.
+2. Point the camera at an object and press the button.
    - The green light will turn off.
-3. The model now generates tokens.
+   - The camera will take a picture and detect an object.
+3. The model now generates tokens starting with a prompt based on the object.
    - The results are streamed to the serial port.
    - This happens at a rate of ~2.5 tokens per second.
 4. Generation stops after the end token or maximum steps.
--- main.cc
+++ main.cc
@@ -1,15 +1,26 @@
+#include <sstream>
+#include <string>
 #include <vector>
 
 #include "libs/base/filesystem.h"
 #include "libs/base/gpio.h"
 #include "libs/base/led.h"
 #include "libs/base/timer.h"
+#include "libs/camera/camera.h"
+#include "libs/tensorflow/classification.h"
+#include "libs/tensorflow/utils.h"
+#include "libs/tpu/edgetpu_manager.h"
+#include "libs/tpu/edgetpu_op.h"
 #include "third_party/freertos_kernel/include/FreeRTOS.h"
 #include "third_party/freertos_kernel/include/task.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_error_reporter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_interpreter.h"
+#include "third_party/tflite-micro/tensorflow/lite/micro/micro_mutable_op_resolver.h"
 
 #include "llama2.h"
 
 using namespace coralmicro;
+using namespace tflite;
 
 // Llama model data paths.
 const char* kLlamaModelPath = "/data/stories15M_q80.bin";
@@ -19,56 +30,209 @@ const char* kLlamaTokenizerPath = "/data/tokenizer.bin";
 const float kTemperature = 1.0f;
 const float kTopP = 0.9f;
 const int kSteps = 256;
-const char* kPrompt = nullptr;
+const char* kPromptPattern = "Once upon a time, there was a ";
 
-// Llama model data.
+// Llama model data structures.
 Transformer transformer;
-std::vector<uint8_t> llama_model_buffer;
+std::vector<uint8_t>* llama_model_buffer;
 int group_size;
 int steps = kSteps;
 Tokenizer tokenizer;
-std::vector<uint8_t> llama_tokenizer_buffer;
+std::vector<uint8_t>* llama_tokenizer_buffer;
 Sampler sampler;
+
+// Vision model data path.
+const char* kVisionModelPath =
+    "/data/tf2_mobilenet_v3_edgetpu_1.0_224_ptq_edgetpu.tflite";
+const char* kVisionLabelsPath = "/data/imagenet_labels.txt";
+
+// Vision model data structures.
+std::vector<uint8_t>* vision_model_buffer;
+std::vector<std::string>* vision_labels;
+const size_t kTensorArenaSize = 512 * 1024;
+STATIC_TENSOR_ARENA_IN_SDRAM(tensor_arena, kTensorArenaSize);
+MicroErrorReporter tf_error_reporter;
+MicroMutableOpResolver<1>* tf_resolver;
+MicroInterpreter* tf_interpreter;
+PerformanceMode kTpuPerformanceMode = PerformanceMode::kLow;  // Fast enough.
+std::shared_ptr<EdgeTpuContext> tpu_context;
+
+// Camera configuration.
+CameraFrameFormat frame_format;
+const int kDiscardFrames = 10;
+const int kClassifierThreshold = 0.0f;
 
 // Debounce interval for the button interrupt.
 const uint64_t kButtonDebounceUs = 50000;
 
+// Loads the Llama model and tokenizer into memory and sets up data structures.
 void LoadLlamaModel() {
   printf(">>> Loading Llama model %s...\n", kLlamaModelPath);
-  int64_t load_start = TimerMillis();
+  int64_t timer_start = TimerMillis();
 
-  build_transformer(&transformer, kLlamaModelPath, &llama_model_buffer,
+  llama_model_buffer = new std::vector<uint8_t>();
+  build_transformer(&transformer, kLlamaModelPath, llama_model_buffer,
                     &group_size);
   if (steps == 0 || steps > transformer.config.seq_len) {
     steps = transformer.config.seq_len;
   }
 
-  build_tokenizer(&tokenizer, kLlamaTokenizerPath, &llama_tokenizer_buffer,
+  llama_tokenizer_buffer = new std::vector<uint8_t>();
+  build_tokenizer(&tokenizer, kLlamaTokenizerPath, llama_tokenizer_buffer,
                   transformer.config.vocab_size);
 
   unsigned long long rng_seed = xTaskGetTickCount();
   build_sampler(&sampler, transformer.config.vocab_size, kTemperature, kTopP,
                 rng_seed);
 
-  int64_t load_end = TimerMillis();
-  float load_s = (load_end - load_start) / 1000.0f;
-  printf(">>> Llama model loading took %.2f s\n", load_s);
+  int64_t timer_stop = TimerMillis();
+  float timer_s = (timer_stop - timer_start) / 1000.0f;
+  printf(">>> Llama model loading took %.2f s\n", timer_s);
 }
 
+// Frees the memory associated with the Llama model.
 void UnloadLlamaModel() {
   printf(">>> Unloading Llama model...\n");
 
   free_sampler(&sampler);
   free_tokenizer(&tokenizer);
   free_transformer(&transformer);
+
+  delete llama_tokenizer_buffer;
+  delete llama_model_buffer;
 }
 
-void TellStory() {
+// Loads the vision model and labels into memory.
+void LoadVisionModel() {
+  printf(">>> Loading vision model %s...\n", kVisionModelPath);
+  int64_t timer_start = TimerMillis();
+
+  // Load the model weights.
+  vision_model_buffer = new std::vector<uint8_t>();
+  if (!LfsReadFile(kVisionModelPath, vision_model_buffer)) {
+    printf("ERROR: Failed to load vision model weights: %s\n",
+           kVisionModelPath);
+    return;
+  }
+
+  // Load the model labels.
+  std::string vision_labels_buffer;
+  vision_labels = new std::vector<std::string>();
+  if (!LfsReadFile(kVisionLabelsPath, &vision_labels_buffer)) {
+    printf("ERROR: Failed to load vision model labels: %s\n",
+           kVisionLabelsPath);
+    return;
+  }
+  std::istringstream labels_stream(vision_labels_buffer);
+  std::string label;
+  while (std::getline(labels_stream, label)) {
+    vision_labels->push_back(label);
+  }
+
+  // Initialize the TPU.
+  // TODO: Investigate why the TPU context can't just live in the TakePicture()
+  //       scope (which would turn off the TPU while it's not being used).
+  tpu_context = EdgeTpuManager::GetSingleton()->OpenDevice(kTpuPerformanceMode);
+  if (!tpu_context) {
+    printf("ERROR: Failed to get TPU context\n");
+    return;
+  }
+
+  // Initialize the TF Lite interpreter.
+  tf_resolver = new MicroMutableOpResolver<1>();
+  tf_resolver->AddCustom(kCustomOp, RegisterCustomOp());
+  tf_interpreter =
+      new MicroInterpreter(GetModel(vision_model_buffer->data()),
+                           *tf_resolver, tensor_arena, kTensorArenaSize,
+                           &tf_error_reporter);
+  if (tf_interpreter->AllocateTensors() != kTfLiteOk) {
+    printf("ERROR: Failed to allocate tensors\n");
+    return;
+  }
+
+  // Initialize the camera capture.
+  auto* input_tensor = tf_interpreter->input_tensor(0);
+  frame_format.fmt = CameraFormat::kRgb;
+  frame_format.filter = CameraFilterMethod::kBilinear;
+  frame_format.rotation = CameraRotation::k270;
+  frame_format.width = input_tensor->dims->data[1];
+  frame_format.height = input_tensor->dims->data[2];
+  frame_format.preserve_ratio = false;
+  frame_format.buffer = GetTensorData<uint8_t>(input_tensor);
+  frame_format.white_balance = true;
+
+  int64_t timer_stop = TimerMillis();
+  float timer_s = (timer_stop - timer_start) / 1000.0f;
+  printf(">>> Vision model loading took %.2f s\n", timer_s);
+}
+
+// Frees the memory associated with the vision model.
+void UnloadVisionModel() {
+  printf(">>> Unloading vision model...\n");
+
+  delete tf_resolver;
+  delete tf_interpreter;
+  delete vision_model_buffer;
+  delete vision_labels;
+}
+
+// Takes a picture and returns the label of the main detected object.
+std::string TakePicture() {
+  printf(">>> Taking picture...\n");
+  int64_t timer_start = TimerMillis();
+
+  // Turn on the camera.
+  if (!CameraTask::GetSingleton()->SetPower(true)) {
+    printf("ERROR: Failed to power on camera\n");
+    return "";
+  }
+  if (!CameraTask::GetSingleton()->Enable(CameraMode::kStreaming)) {
+    printf("ERROR: Failed to enable camera\n");
+    return "";
+  }
+
+  // Discard some frames to get a recent one and give auto exposure more time.
+  CameraTask::GetSingleton()->DiscardFrames(10);
+  if (!CameraTask::GetSingleton()->GetFrame({frame_format})) {
+    printf("ERROR: Failed to take picture\n");
+    return "";
+  }
+
+  // Turn off the camera.
+  CameraTask::GetSingleton()->Disable();
+  CameraTask::GetSingleton()->SetPower(false);
+
+  // Run the object vision model on the image.
+  if (tf_interpreter->Invoke() != kTfLiteOk) {
+    printf("ERROR: Failed to detect objects\n");
+    return "";
+  }
+
+  // Pick just the first result.
+  auto results = tensorflow::GetClassificationResults(tf_interpreter,
+                                                      kClassifierThreshold, 1);
+  if (results.empty()) {
+    printf("ERROR: No objects detected\n");
+    return "";
+  }
+  auto result = results[0];
+  std::string label = vision_labels->at(result.id);
+  printf(">>> Found %s (%.2f)\n", label.c_str(), result.score);
+
+  int64_t timer_stop = TimerMillis();
+  float timer_s = (timer_stop - timer_start) / 1000.0f;
+  printf(">>> Picture taking took %.2f s\n", timer_s);
+
+  return label;
+}
+
+// Generates a story beginning with the specified prompt.
+void TellStory(std::string prompt) {
   printf(">>> Generating tokens...\n");
 
   float tokens_s;
-  generate(&transformer, &tokenizer, &sampler, kPrompt, steps, group_size,
-           &tokens_s);
+  generate(&transformer, &tokenizer, &sampler, prompt.c_str(), steps,
+           group_size, &tokens_s);
 
   printf(">>> Averaged %.2f tokens/s\n", tokens_s);
 }
@@ -82,10 +246,11 @@ extern "C" [[noreturn]] void app_main(void* param) {
       [handle = xTaskGetCurrentTaskHandle()]() { xTaskResumeFromISR(handle); },
       kButtonDebounceUs);
 
-  // Load the model while showing the status LED.
+  // Load the models while showing the status LED.
   LedSet(Led::kStatus, true);
   LedSet(Led::kUser, false);
   LoadLlamaModel();
+  LoadVisionModel();
 
   while (true) {
     // Wait for a button press while showing the user LED.
@@ -94,12 +259,26 @@ extern "C" [[noreturn]] void app_main(void* param) {
     vTaskSuspend(nullptr);
     // Continuing here after the button interrupt.
 
+    // Take a picture while (automatically) showing the camera LED. The result
+    // is the label of the main detected object.
+    LedSet(Led::kStatus, false);
+    LedSet(Led::kUser, false);
+    std::string label = TakePicture();
+
+    // The label might have multiple comma-separated parts. Pick the first one
+    // and combine it with the prompt.
+    std::istringstream label_stream(label);
+    std::getline(label_stream, label, ',');
+    std::string prompt = kPromptPattern;
+    prompt += label;
+
     // Tell a story while showing the status LED.
     LedSet(Led::kStatus, true);
     LedSet(Led::kUser, false);
-    TellStory();
+    TellStory(prompt);
   }
 
   // Unreachable in regular operation. The model stays in memory.
   UnloadLlamaModel();
+  UnloadVisionModel();
 }
